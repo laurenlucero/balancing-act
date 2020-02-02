5ab569531dfdd443fed5b86f8bb6e5780ac15057
“Lauren <lucerolaurenm@gmail.com>
2020-02-02 11:56:56 -0700

    Add Javascript to highlight menu items

--- main.js
+++ main.js
@@ -8,3 +8,30 @@ closeButton.addEventListener('click', welcomeDisappear);
 function welcomeDisappear() {
   closeWelcome.style.display = "none";
 }
+
+var main = document.querySelector('main');
+var menu1 = document.querySelector('.menu1');
+var menu2 = document.querySelector('.menu2');
+var menu3 = document.querySelector('.menu3');
+
+menu1.addEventListener('click', highlightMenu1);
+menu2.addEventListener('click', highlightMenu2);
+menu3.addEventListener('click', highlightMenu3);
+
+function highlightMenu1() {
+  menu2.classList.remove('.highlight-menu');
+  menu3.classList.remove('.highlight-menu');
+  menu1.classList.add('.highlight-menu');
+}
+
+function highlightMenu2() {
+  menu1.classList.remove('.highlight-menu');
+  menu3.classList.remove('.highlight-menu');
+  menu2.classList.add('.highlight-menu');
+}
+
+function highlightMenu3() {
+  menu1.classList.remove('.highlight-menu');
+  menu2.classList.remove('.highlight-menu');
+  menu3.classList.add('.highlight-menu');
+}
