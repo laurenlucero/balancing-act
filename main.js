db4ffc9d34ecb3a9127cb3ecd7a3a2b14f617a8f
“Lauren <lucerolaurenm@gmail.com>
2020-02-02 16:10:07 -0700

    Wrote transactions page HTML and JavaScript

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var menu1 = document.querySelector('.menu1');
 var menu2 = document.querySelector('.menu2');
 var menu3 = document.querySelector('.menu3');
 var dashboard = document.querySelector('.dashboard');
+var transactions = document.querySelector('.transactions');
 
 closeButton.addEventListener('click', welcomeDisappear);
 
@@ -21,14 +22,20 @@ function highlightMenu1() {
   menu2.classList.remove('highlight-menu');
   menu3.classList.remove('highlight-menu');
   menu1.classList.add('highlight-menu');
-  dashboard.classList.remove('is-hidden');
+  dashboard.hidden = false;
+  transactions.hidden = true;
+  // dashboard.classList.remove('is-hidden');
+  // transactions.classList.add('is-hidden');
 }
 
 function highlightMenu2() {
   menu1.classList.remove('highlight-menu');
   menu3.classList.remove('highlight-menu');
   menu2.classList.add('highlight-menu');
-  dashboard.classList.add('is-hidden');
+  dashboard.hidden = true;
+  transactions.hidden = false;
+  // dashboard.classList.add('is-hidden');
+  // transactions.classList.remove('is-hidden');
 }
 
 function highlightMenu3() {
